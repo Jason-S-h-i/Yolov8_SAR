68ae01d2aa302f0621666086d490cee27be550a9
Jason_Laptop <1441098779@qq.com>
2024-02-27 19:58:11 +0800

    2st train

--- SAR_yolo.py
+++ SAR_yolo.py
@@ -5,12 +5,13 @@ from ultralytics import YOLO
 # defalut.yaml imgsz change
 
 project = 'runs'  # 保存训练的输出
-name = 'detect\\train1_0212'  # project下的子文件夹 保存logs和训练的输出
-train_model = '..\\model\\yolov8s.pt'
-epochs = 300
+train_name = 'detect\\2_0228_train'  # project下的子文件夹 保存logs和训练的输出
+val_name = 'detect\\2_0228_val'  # project下的子文件夹 保存logs和训练的输出
+train_model = '..\\model\\yolov8n.pt'
+epochs = 1000
 imgsz = 1500
-batch_size = 8
-save_period = 10  # 保存模型的频次
+batch_size = 24
+save_period = 20  # 保存模型的频次
 
 # 从头开始创建一个新的YOLO模型
 # model = YOLO('yolov8n.yaml')
@@ -23,10 +24,10 @@ if __name__ == "__main__":  # 多进程要放入main函数中
 
     # 使用“coco128.yaml”数据集训练模型3个周期
     results = model.train(data='SAR-AIRcraft-1.0-yolo.yaml', epochs=epochs, batch=batch_size, imgsz=imgsz, device=0,
-                          save_period=save_period, project=project, name=name, plots=True)
+                          save_period=save_period, project=project, name=train_name, plots=True)
 
     # 评估模型在验证集上的性能
-    val_results = model.val()
+    # val_results = model.val(data='SAR-AIRcraft-1.0-yolo.yaml', batch=batch_size, imgsz=imgsz, device=0, project=project, name=val_name, plots=True)
 
     # 使用模型对图片进行目标检测
     # test_results = model(path_test_img)
